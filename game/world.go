fbd73d90ea71194df042c870f9c3c30f47a5c2f8
cbeimers113 <chrisbeimers113@gmail.com>
2023-05-11 23:49:51 -0400

    Add RemoveEntity function

--- game/world.go
+++ game/world.go
@@ -20,6 +20,41 @@ func AddEntityTo(node *core.Node, entity *graphic.Mesh) {
 	Entities[len(Entities)] = node.ChildAt(len(node.Children()) - 1).GetNode()
 }
 
+// Remove an entity from the world
+func RemoveEntity(entity Entity) {
+	for _, child := range entity.Children(){
+		RemoveEntity(child.GetNode())
+	}
+
+	dropEntity(entity)
+	entity.SetVisible(false)
+	entity.RemoveAll(true)
+	entity.Parent().GetNode().Remove(entity)
+	entity.DisposeChildren(true)
+	entity.Dispose()
+}
+
+// Remove an entity from the Entities list
+func dropEntity(entity Entity) {
+	index := -1
+
+	// Find the entity's index in the Entities list
+	for i, ent := range Entities {
+		if ent == entity {
+			index = i
+			break
+		}
+	}
+
+	// If the entity is in the Entities list, remove it and shift all the entities above it down the list
+	if index >= 0 {
+		for i := index + 1; i < len(Entities); i++ {
+			Entities[i - 1] = Entities[i]
+		}
+		delete(Entities, len(Entities) - 1)
+	}
+}
+
 // Load the world into the scene
 func LoadWorld() {
 	// Sun
